91fec21348f167308c09740a6219b50806912861
quocdk <d.kienquoc@outlook.com>
2024-09-16 23:04:36 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// class component
+// function component
+import React from 'react';
+
+class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+            <div>
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
